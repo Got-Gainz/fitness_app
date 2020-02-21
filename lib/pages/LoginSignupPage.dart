899fae2c1aa61fa057a70973925d8ec182a7e503
Tony <tonynguyen0925@gmail.com>
2020-02-21 13:36:48 -0800

    backend for login page and temp UI

---
+++ lib/pages/LoginSignupPage.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupPage extends StatefulWidget {
+  @override
+  _LoginSignupPage createState() => _LoginSignupPage();
+}
+
+class _LoginSignupPage extends State<LoginSignupPage> {
+  final _formKey = new GlobalKey<FormState>();
+  //TODO: Add iOS .plist for FireBase
+  //Variables
+  bool _isLoading;
+  bool _isLoginForm;
+
+  String _email;
+  String _password;
+  String _errorMessage;
+
+  @override
+  void initState() {
+    _errorMessage = '';
+    _isLoading = false;
+    _isLoginForm = true;
+    super.initState();
+  }
+
+  void validateAndSubmit() {}
+
+  void toggleFormMode() {
+    resetForm();
+    setState(
+      () {
+        _isLoginForm = !_isLoginForm;
+      },
+    );
+  }
+
+  void resetForm() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+  }
+
+  Widget showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return Text(
+        _errorMessage,
+        style: TextStyle(
+          fontSize: 13.0,
+          color: Colors.red,
+          height: 1.0,
+          fontWeight: FontWeight.w300,
+        ),
+      );
+    } else {
+      return Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.green, //TODO: Finalize Color
+      ),
+      body: Stack(
+        children: <Widget>[
+          showForm(),
+          showCircularProgress(),
+        ],
+      ),
+    );
+  }
+
+  Widget showForm() {
+    return Container(
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            showLogo(),
+            showEmailInput(),
+            showPasswordInput(),
+            showPrimaryButton(),
+            showSecondaryButton(),
+            showErrorMessage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget showCircularProgress() {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Container(
+      height: 0.0,
+      width: 0.0,
+    );
+  }
+
+  Widget showLogo() {
+    return Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.network(
+              'https://pixy.org/images/placeholder.png'), //TODO: Placeholder
+        ),
+      ),
+    );
+  }
+
+  Widget showEmailInput() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(25.0, 100.0, 25.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          icon: Icon(
+            Icons.mail,
+            color: Colors.greenAccent, //TODO: Finalize Color
+          ),
+        ),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPasswordInput() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          icon: Icon(
+            Icons.lock,
+            color: Colors.greenAccent, //TODO: Finalize Color
+          ),
+        ),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPrimaryButton() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(50.0, 45.0, 50.0, 0.0),
+      child: SizedBox(
+        height: 40.0,
+        child: RaisedButton(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          color: Colors.green, //TODO: Finalize Color
+          child: Text(
+            _isLoginForm ? 'Login' : 'Create Account',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: validateAndSubmit,
+        ),
+      ),
+    );
+  }
+
+  Widget showSecondaryButton() {
+    return FlatButton(
+      child: Text(
+        _isLoginForm ? 'Create Account' : 'Have an account? Sign in',
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+      onPressed: toggleFormMode,
+    );
+  }
+}
