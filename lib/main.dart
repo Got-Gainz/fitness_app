51775577ded94cfae513b27ba2798941012b68a6
Tony <tonynguyen0925@gmail.com>
2020-02-27 11:35:19 -0800

    added splashScreeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:fitness_app/pages/LoginSignupPage.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_splash/animated_splash.dart';
 
 void main() => runApp(FitnessApp());
 
+Function duringSplash = () {
+  return 1;
+};
+
+Map<int, Widget> op = {1: Home()};
+
 class FitnessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AnimatedSplash(
+        imagePath: 'assets/splashScreen.gif',
+        home: Home(),
+        customFunction: duringSplash,
+        duration: 2500,
+        type: AnimatedSplashType.BackgroundProcess,
+        outputAndHome: op,
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
