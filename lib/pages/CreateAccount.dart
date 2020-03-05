1409baeba6334667efd79d5e3e7c13eb2da6172f
Jane Cabanayan <60668425+ayeejane@users.noreply.github.com>
2020-03-05 10:50:28 -0800

    create account

---
+++ lib/pages/CreateAccount.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class CreateAccount extends StatefulWidget {
+  @override
+  _CreateAccount createState() => _CreateAccount();
+}
+
+class _CreateAccount extends State<CreateAccount> {
+  final _formKey = new GlobalKey<FormState>();
+
+  //Variables
+
+  }
+
